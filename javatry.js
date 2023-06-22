72379f8b6d0cd7ab31f2d4eccffa6ba865118f13
Picassabarata <you@example.com>
2023-06-22 13:32:21 +1000

    Submitting folder

---
+++ javatry.js
@@ -0,0 +1,58 @@
+var x = 10;
+console.log(x);
+
+function example(){
+    var y = 20;
+    console.log(y);
+
+    if (true) {
+        var z = 30;
+        console.log(z);
+    }
+
+ console.log(z);
+}
+
+example();
+
+console.log(x);
+
+
+var age = 26;
+var name = "candela";
+
+console.log("Age: " + age);
+console.log("Name: " + name);
+
+age = 30;
+name = "Candela";
+
+console.log("Age updated: " + age);
+console.log("Name is the same: " + name); 
+
+
+var age = 25;
+var temp = -10.5;
+
+
+var name = "John";
+var message = "Hello, World";
+
+var isStudent = true;
+var hasCar = false;
+
+var fruits = ["apple", "banana", "orange"];
+
+
+var person = {
+    name: "john",
+    age: 30,
+    isStudent: true
+};
+
+console.log("Age: " + age);
+console.log("Name is " + name);
+console.log("Is Student: " + isStudent);
+console.log("Fruits:" + fruits);
+console.log("Person: " + person);
+
